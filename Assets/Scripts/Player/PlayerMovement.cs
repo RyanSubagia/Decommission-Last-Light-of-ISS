7e81ffd7e44281a0c990d05d1ade7be5314bb8f3
yan <150877487+RyanSubagia@users.noreply.github.com>
2026-01-22 07:35:14 +0700

    Lha wong saya suka kok

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,16 +7,29 @@ public class PlayerMovement : MonoBehaviour
     [Header("Movement Settings")]
     [SerializeField] private float moveSpeed = 5f;
 
+    [Header("Jump Settings")]
+    [SerializeField] private float jumpForce = 7f;
+
+    [Header("Ground Check")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius = 0.1f;
+    [SerializeField] private LayerMask groundLayer;
+
     [Header("Components")]
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Animator animator;
     [SerializeField] private SpriteRenderer spriteRenderer;
 
     private float horizontalInput;
+    private bool isGrounded;
+    private bool jumpRequested;
+    private int facingDirection = 1; 
 
     private const int STATE_IDLE = 0;
     private const int STATE_WALK_RIGHT = 1;
     private const int STATE_WALK_LEFT = 2;
+    private const int STATE_JUMP_RIGHT = 3;
+    private const int STATE_JUMP_LEFT = 4;
 
     private void Awake()
     {
@@ -29,15 +42,43 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
 
-        int moveState = STATE_IDLE;
-
         if (horizontalInput > 0.01f)
         {
-            moveState = STATE_WALK_RIGHT;
+            facingDirection = 1;
         }
         else if (horizontalInput < -0.01f)
         {
-            moveState = STATE_WALK_LEFT;
+            facingDirection = -1;
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            jumpRequested = true;
+        }
+
+        int moveState = STATE_IDLE;
+
+        if (!isGrounded)
+        {
+            if (facingDirection == 1)
+            {
+                moveState = STATE_JUMP_RIGHT;
+            }
+            else
+            {
+                moveState = STATE_JUMP_LEFT;
+            }
+        }
+        else
+        {
+            if (horizontalInput > 0.01f)
+            {
+                moveState = STATE_WALK_RIGHT;
+            }
+            else if (horizontalInput < -0.01f)
+            {
+                moveState = STATE_WALK_LEFT;
+            }
         }
 
         animator.SetInteger("moveState", moveState);
@@ -45,6 +86,20 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+        if (groundCheck != null)
+        {
+            isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        }
+
+        Vector2 velocity = rb.velocity;
+        velocity.x = horizontalInput * moveSpeed;
+
+        if (jumpRequested)
+        {
+            velocity.y = jumpForce;
+            jumpRequested = false;
+        }
+
+        rb.velocity = velocity;
     }
 }
