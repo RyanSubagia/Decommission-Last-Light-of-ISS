75f508bd302e3796b1ddf7794fcfce677c7e3199
yan <150877487+RyanSubagia@users.noreply.github.com>
2026-01-23 17:07:40 +0700

    note

--- Assets/Scripts/World/GameManager.cs
+++ Assets/Scripts/World/GameManager.cs
@@ -8,7 +8,7 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance { get; private set; }
 
     [Header("Countdown Settings")]
-    [SerializeField] private float countdownDurationSeconds = 300f; // 5 minutes
+    [SerializeField] private float countdownDurationSeconds = 300f; 
     [SerializeField] private TMP_Text txtxCountdown;
 
     [Header("Fade Settings")]
